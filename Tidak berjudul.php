000f7c0a45f9a6d96f27228087e6656c7c23c21d
Naddd <61293520+Naldhyyy@users.noreply.github.com>
2020-02-21 04:36:27 +0800

    Add files via upload

---
+++ Tidak berjudul.php
@@ -0,0 +1,18 @@
+$ pkg install python && pkg install git
+
+$ git clone https://github.com/marioyhzkiell/bugreport.git 
+
+
+$ cd bugreport
+
+$ chmod 777 bugreport.py
+
+$ python bugreport.py
+
+Screen shoot
+
+
+
+
+
+Goodluck:)
